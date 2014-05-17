34f44f89adc1341851a69175cc2e05700356860e
DeanIsseyegh <issdea@gmail.com>
2014-05-17 15:07:08 +0100

    trying to compile

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ http.createServer(app).listen(app.get('port'), function(){
   pg.connect(process.env.DATABASE_URL, function(err, client) {
 		 var query = client.query('SELECT * FROM hitme_user;');
 		 console.log('**Running Query');
+		 console.log('**query: ' + query);
 		 query.on('last_name', function(row) {
 			console.log('**About to show JSON stringy');
 		   console.log(JSON.stringify(row));
